9a8e986f7768fc1cdd71aefeae883efd0ca185ec
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2026-08-02 19:21:41 +0300

    Add implementation for external API calls

---
+++ src/05-external-api-calls.ts
@@ -0,0 +1,62 @@
+const API_URL = "https://jsonplaceholder.typicode.com/users/1";
+
+type PlaceholderUser = {
+  id: number;
+  name: string;
+  email: string;
+  company: {
+    name: string;
+  };
+}
+
+type PublicUser = {
+  id: number;
+  name: string;
+  email: string;
+  company: string;
+}
+
+function transformUser(rawData: PlaceholderUser): PublicUser {
+  return {
+    id: rawData.id,
+    name: rawData.name,
+    email: rawData.email,
+    company: rawData.company.name,
+  }
+}
+
+async function fetchExternalUser(): Promise<void> {
+  const controller = new AbortController();
+
+  const timeOut = setTimeout(() => {
+    controller.abort();
+  }, 5000);
+
+  try {
+    const response = await fetch(API_URL, {
+      method: "GET",
+      signal: controller.signal
+    });
+
+    if (!response.ok) {
+      console.error(`upstream api failed with status: ${response.status}`);
+      return;
+    }
+
+    const rawUser = (await response.json()) as PlaceholderUser;
+    const user = transformUser(rawUser);
+    console.log(user);
+  } catch (error) {
+    if (error instanceof Error && error.name === "AbortError") {
+      console.error("upstream api request timed out");
+      return;
+    }
+
+    const msg = error instanceof Error ? error.message : "unknown error";
+    console.error("External API call failed: ", msg);
+  } finally {
+    clearTimeout(timeOut);
+  }
+}
+
+fetchExternalUser();
